c00493174319f8d8ab258d13228876bbffe5907f
Jesse <jessebot.git@gmail.com>
2026-07-31 15:51:47 -0700

    remove batch scripts

--- test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-if not exist out ( mkdir out )
-
-clang -std=c89 -O0 -g3 -ffreestanding -pedantic -Wall -Wextra -Weverything -Werror -Wno-unused-function -Wno-unused-parameter -Wno-unused-variable -Wno-unsafe-buffer-usage -Wno-sign-conversion -Wno-undef demo/build.c -o out/build.exe
--- testcpp.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-if not exist out ( mkdir out )
-
-clang++ -std=c++11 -Wno-deprecated demo/build.c -o out/buildcpp.exe
